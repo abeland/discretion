4fc6843dd4242b449ea2118242cd294d94c3d2d3
Abe Land <abe@cs.stanford.edu>
2018-02-02 15:39:12 -0800

    Allow omniscient loading and omnipotent writing as bypass mechanisms in Discretion.

--- lib/discretion/helpers.rb
+++ lib/discretion/helpers.rb
@@ -11,5 +11,29 @@ module Discretion
     def can_see_records?(viewer, *records)
       records.all? { |record| Discretion.can_see_record?(viewer, record) }
     end
+
+    def omnisciently
+      # Calling Proc.new will create a Proc from the implicitly given block to
+      # the current method.
+      # cf. http://ruby-doc.org/core-2.5.0/Proc.html#method-c-new
+      with_viewer(Discretion::OMNISCIENT_VIEWER, &Proc.new)
+    end
+
+    def omnipotently
+      # Calling Proc.new will create a Proc from the implicitly given block to
+      # the current method.
+      # cf. http://ruby-doc.org/core-2.5.0/Proc.html#method-c-new
+      with_viewer(Discretion::OMNIPOTENT_VIEWER, &Proc.new)
+    end
+
+    private
+
+    def with_viewer(viewer)
+      orig_viewer = Discretion.current_viewer
+      Discretion.set_current_viewer(viewer)
+      yield
+    ensure
+      Discretion.set_current_viewer(orig_viewer)
+    end
   end
 end
